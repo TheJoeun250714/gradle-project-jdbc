4daf213c684d8ec5ec0778cc758a5f55622a5ee4
dprdqel055 <dprdqel055@outlook.com>
2026-02-13 10:46:40 +0900

    Initial commit

---
+++ src/main/java/com/my/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.my.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message",
+                "안녕하세요.");
+        return "index";
+    }
+
+}
+
+
+
